4e66d2e35a0f9d1956a299a55b608fb0a2d2d662
Pecuchova <jpecuchova96@gmail.com>
2024-10-15 10:42:28 +0200

    custom fonts

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { SafeAreaView, Text } from "react-native";
+import { getFontFamily } from "./assets/fonts/helper";
 
 const App = () => {
   return (
  <SafeAreaView>
-    <Text>Hello World</Text>
+    <Text 
+    style= {{fontSize: 50, fontFamily: getFontFamily ('Inter_18pt', '100')}}>
+      Hello World</Text>
  </SafeAreaView>
   );
 }
