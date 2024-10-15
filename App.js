e2de7a8d6fca2097bdc26eaf0603995dc9f31679
Pecuchova <jpecuchova96@gmail.com>
2024-10-15 14:13:43 +0200

    font-awesome StyleSheet

--- App.js
+++ App.js
@@ -1,28 +1,23 @@
 import React from "react";
-import { SafeAreaView, TouchableOpacity, View } from "react-native";
+import { SafeAreaView, TouchableOpacity, View, Text } from "react-native";
 import Title from "./components/Title/Title";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
+import globalStyle from "./assets/fonts/styles/globalStyle";
 
 const App = () => {
   return (
  <SafeAreaView>
   <View 
-  style={{
-    marginRight: 17, 
-    marginLeft: 27, 
-    marginTop: 30,
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-  }}>
+  style={globalStyle.header}>
     <Title title={"Let's Explore"}/>
-    <TouchableOpacity style={{
-      padding:14, 
-      borderRadius: 100, 
-      backgroundColor:"#F9FAFB"
-      }}>
-      <FontAwesomeIcon icon={faEnvelope} color= {"#898DAE"} />
+    <TouchableOpacity style={globalStyle.envelopeIcon}>
+      <FontAwesomeIcon icon={faEnvelope} size={20} color= {"#898DAE"} />
+      <View style={globalStyle.messageNumberContainer}>
+        <Text style={globalStyle.messageNumber}>
+          2
+        </Text>
+      </View>
     </TouchableOpacity>
   </View>
  </SafeAreaView>
