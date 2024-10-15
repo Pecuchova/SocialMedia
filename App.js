544125ec41d1f350e10b535333ce98cf061b3343
Pecuchova <jpecuchova96@gmail.com>
2024-10-15 11:58:15 +0200

    font-awesome inline

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
 import React from "react";
-import { SafeAreaView } from "react-native";
+import { SafeAreaView, TouchableOpacity, View } from "react-native";
 import Title from "./components/Title/Title";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
 
 const App = () => {
   return (
  <SafeAreaView>
+  <View 
+  style={{
+    marginRight: 17, 
+    marginLeft: 27, 
+    marginTop: 30,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  }}>
     <Title title={"Let's Explore"}/>
+    <TouchableOpacity style={{
+      padding:14, 
+      borderRadius: 100, 
+      backgroundColor:"#F9FAFB"
+      }}>
+      <FontAwesomeIcon icon={faEnvelope} color= {"#898DAE"} />
+    </TouchableOpacity>
+  </View>
  </SafeAreaView>
   );
 }
